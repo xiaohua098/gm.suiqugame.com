54d053ef665f69626ccad27c9d24f47fbe9b0148
xiaohua098 <1424779356@qq.com>
2017-11-01 17:18:50 +0800

    11.1

--- application/img/controller/Test.php
+++ application/img/controller/Test.php
@@ -12,41 +12,18 @@ use think\Response;
 // header('Access-Control-Allow-Headers:content-type,token'); 
 class Test extends Controller{
 	public   function   test(){
-         // $record=Db::connect('db3')->table('RecordPrivateCost')
-         //          ->where('CostFrom',1)
-         //          ->whereOr('CostFrom',2)
-         //          ->where('UserID',$uid)
-         //          ->where('CostDate','between ',[$start,$end])
-         //          ->where('RecordID','<=',$tid['RecordID'])
-         //          ->order('CostDate','desc')
-         //          ->limit($pagesize)->select();
-         //    //游戏名称和电话号码
-         //        foreach ($record as $k => $v) {
-         //            $record[$k]['phone']=$phone;
-         //            $record[$k]['KindName']=Db::connect('db4')->table('GameKindItem')->where('KindID',$v['KindID'])->value('KindName');
-         //        }
-         // $subsql = Db::connect('db4')->table('GameKindItem')->field('KindID,KindName')->buildSql();
-         // $record=Db::connect('db3')->table('RecordPrivateCost')
-         //          ->alias('a')
-         //          ->join([$subsql=>'b'],'a.KindID=b.KindID','LEFT')
-         //          ->field('a.*,b.KindName')
-         //          ->where('a.CostFrom',1)
-         //          ->whereOr('a.CostFrom',2)
-         //          // ->where('a.UserID',$uid)
-         //          // ->where('a.CostDate','between ',[$start,$end])
-         //          // ->where('a.RecordID','<=',$tid['RecordID'])
-         //          // ->order('a.CostDate','desc')
-         //          // ->limit($pagesize)
-         //          ->limit(2)
-         //          ->select();
-                
-         //  // $record=Db::connect('db3')->query("select a.*,b.KindName from RecordPrivateCost a left join GameKindItem b on a.KindID=b.KindID  order by  CostDate desc ");
-         //    var_dump($record);
-         //    
-         $res=Db::connect('db5')->table('transfer_log')->where('uid',1)->limit(1,2)->select();
-         var_dump($res);
-
-
+        // // 查询多条
+        //  Db::connect('db5')->table('user')
+        //     ->where(['id' => 3])
+        //      ->selectOrFail();
+      try{
+         // 查询单条
+         Db::connect('db5')->table('user')->where('id',3)
+             ->findOrFail();
+      }catch (\Exception $e){
+          var_dump($e);exit;
+      }
+      
   }
 
 
